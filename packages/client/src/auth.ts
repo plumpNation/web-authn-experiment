5331694a651d448d5015bb9955402de99e4792fb
Gavin King <plumpnation@gmail.com>
2024-09-24 18:34:54 +0100

    Add typing

--- packages/client/src/auth.ts
+++ packages/client/src/auth.ts
@@ -1,10 +1,15 @@
-const credInit = {
+const credInit: PasswordCredentialInit = {
   id: "1234",
   name: "Serpentina",
   origin: "https://example.org",
   password: "the last visible dog",
 };
 
-navigator.credentials.create({
-  password: credInit,
-});
+const createPasswordCred = async (): Promise<PasswordCredential | null> =>
+  await navigator.credentials.create({
+    // password can be a DOM <form> or a PasswordCredentialInit
+    password: credInit,
+  }) as PasswordCredential;
+
+const getPasswordCred = async (): Promise<PasswordCredential | null> =>
+  await navigator.credentials.get() as PasswordCredential;
