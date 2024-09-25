c8654f0c24b447d0c903d140609a43c28215d776
Gavin King <plumpnation@gmail.com>
2024-09-25 11:58:18 +0100

    WIP

--- packages/client/src/auth.ts
+++ packages/client/src/auth.ts
@@ -13,3 +13,30 @@ const createPasswordCred = async (): Promise<PasswordCredential | null> =>
 
 const getPasswordCred = async (): Promise<PasswordCredential | null> =>
   await navigator.credentials.get() as PasswordCredential;
+
+const randomStringFromServer = Math.random().toString(36).substring(2, 15);
+const challenge = Uint8Array.from(randomStringFromServer, c => c.charCodeAt(0));
+
+const publicKeyCredentialCreationOptions: PublicKeyCredentialCreationOptions = {
+  challenge,
+  rp: {
+      name: "Plumpatron",
+      id: "localhost",
+  },
+  user: {
+      id: Uint8Array.from("UZSL85T9AFC", c => c.charCodeAt(0)),
+      name: "gavin.king@cgi.com",
+      displayName: "Gavin King",
+  },
+  pubKeyCredParams: [{alg: -7, type: "public-key"}],
+  authenticatorSelection: {
+      authenticatorAttachment: "cross-platform",
+  },
+  timeout: 60000,
+  attestation: "direct"
+};
+
+const getPublicKeyCred = async () =>
+  await navigator.credentials.create({
+    publicKey: publicKeyCredentialCreationOptions
+  });
--- packages/server/src/vanilla.mts
+++ packages/server/src/vanilla.mts
@@ -13,4 +13,11 @@ app.get('/', (_, res) => {
   res.send('Hello World');
 });
 
+app.get('/webauthn/challenge', (_, res) => {
+  const randomStringFromServer = Math.random().toString(36).substring(2, 15);
+  const challenge = Uint8Array.from(randomStringFromServer, c => c.charCodeAt(0));
+
+  res.json({ challenge });
+});
+
 server.listen(3000, () => { console.log('listening on 3000') });
